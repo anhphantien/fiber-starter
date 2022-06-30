1feb2e3df0bb665d5f86bb950202a16b65bbaa8f
Anh Phan <phanlong315@gmail.com>
2022-06-30 17:43:45 +0700

    .

--- utils/transformer.go
+++ utils/transformer.go
@@ -8,8 +8,10 @@ func ConvertToID(v any) uint64 {
 		return *_v
 	case float64:
 		return uint64(v.(float64))
-	default: // string
+	case string:
 		n, _ := strconv.ParseUint(v.(string), 10, 64)
 		return n
+	default:
+		return v.(uint64)
 	}
 }

37b99b8ca2f1089a3b58f4a273a75bc15ea37f3f
Anh Phan <phanlong315@gmail.com>
2022-07-06 10:42:46 +0700

    anh đã rất nhớ em

--- handlers/file.handler.go
+++ handlers/file.handler.go
@@ -15,7 +15,7 @@ type FileHandler struct{}
 // @Tags    file
 // @Summary Upload a file
 // @Param   file                formData file false " "
-// @Success 201                 object   response.Response{}
+// @Success 201                 object   response.Response{data=boolean}
 // @Router  /api/v1/file/upload [POST]
 func (h FileHandler) Upload(c *fiber.Ctx) error {
 	file, err := c.FormFile("file")
@@ -49,5 +49,7 @@ func (h FileHandler) Upload(c *fiber.Ctx) error {
 
 	// c.SaveFile(file, "./"+file.Filename)
 
-	return response.WriteJSON(c, response.Response{})
+	return response.WriteJSON(c, response.Response{
+		Data: true,
+	})
 }
